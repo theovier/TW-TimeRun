6806621c29ab745bfa8dd7ec161bd0be9e961ba6
Theo H <Theo@Harkenbusch.com>
2018-08-13 18:33:20 +0200

    balance: slowed hammer bot attack down

--- src/game/server/entities/bots/hammerbot.cpp
+++ src/game/server/entities/bots/hammerbot.cpp
@@ -5,6 +5,7 @@ CHammerbot::CHammerbot(CGameWorld *pWorld) : CBotCharacter(pWorld) {
 	CBotCharacter::CBotCharacter(pWorld);
 	m_MaxHealth = m_Health = GameServer()->Tuning()->m_HammerBotLife;
 	m_aWeapons[WEAPON_GUN].m_Got = false;
+	m_AttackSpeed = 2.0f;
 }
 
 
